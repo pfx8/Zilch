5b6e02a6f0c4146d11280c18b032e492ccd86806
Ingenim99 <dashar825@gmail.com>
2018-03-23 02:54:14 +0900

    オートマトンモデルを追加

---
+++ StateMachine.h
@@ -0,0 +1,52 @@
+﻿//*****************************************************************************
+//
+// 有限オートマトン [StateMachine.h]
+//
+// Author : LIAO HANCHEN
+//
+//*****************************************************************************
+#ifndef _STATE_MACHINE_H_
+#define _STATE_MACHINE_H_
+
+#include <iostream>
+
+enum State
+{
+	Stop,
+	Walk,
+	Run,
+};
+
+class Player
+{
+private:
+	string playerName;
+public:
+	Player() = default;
+	Player(string str) :playerName(str) {}
+};
+
+//*****************************************************************************
+//
+// クラス宣言
+//
+//*****************************************************************************
+class StateMachine
+{
+private:
+	const string stateName;
+
+public:
+	StateMachine() = default;
+	StateMachine(string str) :stateName(str) {}
+	~StateMachine() {}
+
+	string getStateName() { return stateName; }
+
+	virtual void Enter(Player player);
+	virtual void Update(Player player);
+	virtual void Exit(Player player);
+}; 
+
+#endif // !_STATE_MACHINE_H_
+
